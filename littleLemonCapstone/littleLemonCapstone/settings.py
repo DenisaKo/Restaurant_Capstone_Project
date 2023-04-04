aaab137ba647a72243486157a6a971e506519440
Denisa Koscak <denisa.koscak@gmail.com>
2023-04-04 16:08:14 +0200

    add mysql database

--- littleLemonCapstone/littleLemonCapstone/settings.py
+++ littleLemonCapstone/littleLemonCapstone/settings.py
@@ -81,12 +81,22 @@ WSGI_APPLICATION = 'littleLemonCapstone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',     
+        'OPTIONS': {  
+            'read_default_file': os.path.join(BASE_DIR, "my.cnf"), 
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }  
+    }  
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
